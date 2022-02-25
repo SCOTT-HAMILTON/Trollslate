e613daead0c0a3db06b187481def75f1c940067e
SCOTT-HAMILTON <sgn.hamilton+github@protonmail.com>
2022-02-25 18:22:25 +0100

    Move to kotlin dsl

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,15 +2,10 @@ buildscript {
     repositories {
         google()
         mavenCentral()
-//        maven("https://kotlin.bintray.com/kotlinx")
-    }
-    ext {
-        this.c
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.1.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-//        classpath("com.karumi:shot:5.10.3")
     }
 }
 
@@ -22,7 +17,6 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-//        maven("https://kotlin.bintray.com/kotlinx/")
         @Suppress("JcenterRepositoryObsolete") // Required by dokka plugin
         jcenter()
     }

08c4ef5facacf342fe475246761b5bb628bde2e0
SCOTT-HAMILTON <sgn.hamilton+github@protonmail.com>
2022-03-26 22:08:57 +0100

    Red text when unavailable

--- app/src/main/java/org/scotthamilton/trollslate/ui/TrollTextField.kt
+++ app/src/main/java/org/scotthamilton/trollslate/ui/TrollTextField.kt
@@ -8,9 +8,11 @@ import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.*
@@ -21,11 +23,65 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.scotthamilton.trollslate.data.FontData
+import org.scotthamilton.trollslate.utils.TypedRange
+import org.scotthamilton.trollslate.utils.groupRange
+import org.scotthamilton.trollslate.utils.indivRange
 
 data class TrollTextFieldData(var text: MutableState<String>, val showError: MutableState<Boolean>)
 
 fun defaultTrollTextFieldData(): TrollTextFieldData =
     TrollTextFieldData(text = mutableStateOf(""), showError = mutableStateOf(true))
+
+private fun visualTransformAnnotate(text: String) : AnnotatedString {
+    val utext = text.uppercase()
+    val badRanges =
+        listOf(-1 to -1) +
+        utext.mapIndexed { index, c ->
+            index to c
+        }.filter {
+            it.second !in FontData.lettersCodonTable.keys
+        }.fold(listOf<Pair<Int, Int>>()) { a, c ->
+            if (a.isEmpty()) {
+                listOf(c.first to c.first)
+            } else {
+                val last = a.last()
+                if (c.first - 1 == last.second) {
+                    a.dropLast(1) + listOf(last.copy(second = c.first))
+                } else {
+                    a + listOf(c.first to c.first)
+                }
+            }
+        } + listOf(utext.length to utext.length)
+    val goodRanges =
+        badRanges
+            .foldRight((0 to 0) to mutableListOf<Pair<Int, Int>>()) { t, r ->
+                val prev = r.first
+                if (prev != 0 to 0) {
+                    val newstart = t.second + 1
+                    val newend = r.first.first - 1
+                    if (newend >= newstart) {
+                        r.second.add(newstart to newend)
+                    }
+                }
+                t to r.second
+            }
+            .second
+    val ranges = (goodRanges + badRanges.dropLast(1).drop(1)).sortedBy { it.first }
+    println("Utext=`$utext`, error indices=$ranges")
+
+    return AnnotatedString.Builder("").apply {
+//        append(utext)
+        var isBad = badRanges.size >= 2 && badRanges[1].first == 0
+        ranges.forEach {
+            pushStyle(
+                SpanStyle(color = if (isBad) Color.Red else Color.Black)
+            )
+            append(utext.slice(IntRange(it.first, it.second)))
+            pop()
+            isBad = !isBad
+        }
+    }.toAnnotatedString()
+}
 
 @Composable
 fun TrollTextField(data: TrollTextFieldData, onValueChanged: suspend (CoroutineScope) -> Unit) {
@@ -43,7 +99,7 @@ fun TrollTextField(data: TrollTextFieldData, onValueChanged: suspend (CoroutineS
         },
         visualTransformation = { annotatedString ->
             TransformedText(
-                AnnotatedString(annotatedString.text.uppercase()),
+                visualTransformAnnotate(annotatedString.text),
                 OffsetMapping.Identity
             )
         },
@@ -61,7 +117,7 @@ fun TrollTextField(data: TrollTextFieldData, onValueChanged: suspend (CoroutineS
             )
         },
         colors =
-            TextFieldDefaults.textFieldColors(backgroundColor = MaterialTheme.colorScheme.primary),
+        TextFieldDefaults.textFieldColors(backgroundColor = MaterialTheme.colorScheme.primary),
         textStyle = TextStyle(fontWeight = FontWeight.Bold, fontSize = 30.sp)
     )
 }

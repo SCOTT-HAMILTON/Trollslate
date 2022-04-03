da598fa0bf9b6c870062e71ed7abc4b5b8348883
SCOTT-HAMILTON <sgn.hamilton+github@protonmail.com>
2022-04-03 21:01:52 +0200

    Change releases versionning

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -2,6 +2,6 @@ object Config {
     const val minSdkVersion = 21
     const val compileSdkVersion = 31
     const val targetSdkVersion = 31
-    const val version = 3042022
+    const val version = 30000000
     const val versionName = "Botticelli"
 }
